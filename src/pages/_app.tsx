666bb81d4737efd24455ccfcdad49c0a5ba55848
yumemi-omi <hiro.041310402.kt@gmail.com>
2022-08-13 11:40:19 +0900

    fix: remove global style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,10 @@
 import { ChakraProvider } from "@chakra-ui/react";
 
-import { GlobalStyles } from "@/styles/Globals";
-
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <>
-      <GlobalStyles />
       <ChakraProvider>
         <Component {...pageProps} />
       </ChakraProvider>
