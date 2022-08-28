22c97d8dfa5b4c1fa918e23a5b2f03c8363e1bb1
yumemi-omi <hiro.041310402.kt@gmail.com>
2022-08-28 16:12:05 +0900

    chore: change eslint config file name

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 const config = {
   "*.@(ts|tsx)": "bash -c tsc",
-  "*.@(js|ts|tsx)": "eslint --fix --config eslint.config.js",
+  "*.@(js|ts|tsx)": "eslint --fix --config .eslintrc.js",
   "**/__tests__/?(*.)+(spec|test).[jt]s?(x)": "jest --ci",
 };
 
