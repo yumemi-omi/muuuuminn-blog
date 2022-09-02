c65a89e4392da2c30b4c309191090b26a2bf9fba
yumemi-omi <hiro.041310402.kt@gmail.com>
2022-09-03 02:46:59 +0900

    fix: change base width

--- src/shared/components/BasicLayout/BasicLayout.tsx
+++ src/shared/components/BasicLayout/BasicLayout.tsx
@@ -2,15 +2,21 @@ import { FC } from "react";
 
 import { Footer } from "@/shared/components/Footer";
 import { Header } from "@/shared/components/Header";
-import { Box } from "@/shared/libs/chakra/components/Box";
-import { Grid, GridProps } from "@/shared/libs/chakra/components/Grid";
+import { Box, Grid, GridProps } from "@/shared/libs/chakra";
 
 type BasicLayoutProps = GridProps;
 
 export const BasicLayout: FC<BasicLayoutProps> = (props) => {
   const { children } = props;
   return (
-    <Grid templateRows={"auto 1fr auto"} templateColumns={"100%"} minHeight={"100vh"}>
+    <Grid
+      // TODO: need responsive
+      width={"1400px"}
+      marginX={"auto"}
+      templateRows={"auto 1fr auto"}
+      templateColumns={"100%"}
+      minHeight={"100vh"}
+    >
       <Header />
       <Box>{children}</Box>
       <Footer />
