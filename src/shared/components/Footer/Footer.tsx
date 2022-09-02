45e6d60a3cf98599937d87f08a945451dafcac7e
yumemi-omi <hiro.041310402.kt@gmail.com>
2022-09-03 03:18:53 +0900

    feat: implement footer ui

--- src/shared/components/Footer/Footer.tsx
+++ src/shared/components/Footer/Footer.tsx
@@ -1,8 +1,23 @@
-import { Box, BoxProps } from "@chakra-ui/react";
+import { Center, Text } from "@chakra-ui/react";
 import { FC } from "react";
 
-type FooterProps = BoxProps
+import { Flex, FlexProps } from "@/shared/libs/chakra";
+import { useTranslation } from "@/shared/libs/i18n/hooks/useTranslation";
+import { ChakraNextLink } from "@/shared/libs/next";
+
+type FooterProps = FlexProps;
 
 export const Footer: FC<FooterProps> = () => {
-  return <Box>Footer</Box>;
+  const { t } = useTranslation();
+  return (
+    <Center>
+      <Flex alignItems={"center"} paddingY={"4"}>
+        <ChakraNextLink href={"/"}>
+          <Text fontWeight={"bold"} fontSize={"xl"}>
+            {t.PAGE.HOME}
+          </Text>
+        </ChakraNextLink>
+      </Flex>
+    </Center>
+  );
 };
