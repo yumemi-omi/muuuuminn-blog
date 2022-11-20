92f336cb03f73ebd9ff144974cc80b756aed34f3
yumemi-omi <hiro.041310402.kt@gmail.com>
2022-11-20 09:12:52 +0900

    feat: shared element transition for post card

--- src/features/post/components/PostDetail/PostDetail.tsx
+++ src/features/post/components/PostDetail/PostDetail.tsx
@@ -5,8 +5,9 @@ import { PostDate } from "@/features/post/components/PostDate";
 import { PostThumbnail } from "@/features/post/components/PostThumbnail";
 import { TagList } from "@/features/post/components/TagList";
 import { PostType } from "@/features/post/type/post";
-import { Box, BoxProps, HStack, Stack, VStack } from "@/libs/chakra";
+import { Box, BoxProps, HStack, VStack } from "@/libs/chakra";
 import { Text } from "@/libs/chakra";
+import { MotionChakraDiv } from "@/libs/framerMotion";
 import { RichMarkdownContent } from "@/shared/components/RichMarkdownContent";
 
 type PostDetailProps = {
@@ -17,23 +18,31 @@ export const PostDetail: FC<PostDetailProps> = ({ postDetail, ...rest }) => {
   return (
     <Box overflowX={"hidden"} {...rest}>
       <VStack align={"start"}>
-        <HStack spacing={4}>
-          {postDetail.category && <Category asLink category={postDetail.category} />}
-          <PostDate fontSize={"sm"} date={postDetail.updatedAt} />
-        </HStack>
-        <VStack>
-          <PostThumbnail
-            post={postDetail}
-            imageQuality={75}
-            sizeSet={{
-              width: "200px",
-              height: "200px",
-            }}
-          />
-          <Text noOfLines={3} fontSize={"lg"} fontWeight={"bold"}>
-            {postDetail.title}
-          </Text>
-          <TagList tags={postDetail.tags} flexWrap={"wrap"} />
+        <MotionChakraDiv layoutId={`category_${postDetail.id}`}>
+          <HStack spacing={4}>
+            {postDetail.category && <Category asLink category={postDetail.category} />}
+            <PostDate fontSize={"sm"} date={postDetail.updatedAt} />
+          </HStack>
+        </MotionChakraDiv>
+        <VStack width={"full"}>
+          <MotionChakraDiv layoutId={`thumbnail_${postDetail.id}`}>
+            <PostThumbnail
+              post={postDetail}
+              imageQuality={75}
+              sizeSet={{
+                width: "200px",
+                height: "200px",
+              }}
+            />
+          </MotionChakraDiv>
+          <MotionChakraDiv layoutId={`title_${postDetail.id}`}>
+            <Text noOfLines={3} fontSize={"lg"} fontWeight={"bold"}>
+              {postDetail.title}
+            </Text>
+          </MotionChakraDiv>
+          <MotionChakraDiv layoutId={`tag-list_${postDetail.id}`}>
+            <TagList tags={postDetail.tags} flexWrap={"wrap"} />
+          </MotionChakraDiv>
         </VStack>
       </VStack>
       {postDetail.content && <RichMarkdownContent html={postDetail.content} />}
