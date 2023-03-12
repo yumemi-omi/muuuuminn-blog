53f40a2b690286ce3721c41d634396006842ca9a
yumemi-omi <hiro.041310402.kt@gmail.com>
2023-03-12 14:39:06 +0900

    fix: import path

--- src/pages/post/Post.tsx
+++ src/pages/post/Post.tsx
@@ -6,8 +6,12 @@ import { PostDetailType, PostListType } from "@/features/post/types";
 import { RelatedPostsArea } from "@/features/related-posts/components/RelatedPostsArea";
 import { Stack } from "@/libs/chakra";
 import { useTranslation } from "@/libs/i18n";
-import { ArticleSeo, ArticleSeoProps } from "@/shared/components";
-import { ArticleJsonLd, ArticleJsonLdProps } from "@/shared/components";
+import {
+  ArticleSeo,
+  ArticleSeoProps,
+  ArticleJsonLd,
+  ArticleJsonLdProps,
+} from "@/shared/components";
 
 type PostProps = {
   post: PostDetailType;
