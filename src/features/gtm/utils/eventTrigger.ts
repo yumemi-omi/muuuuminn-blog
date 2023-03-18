cd75a4a9f761e67cc2d6887cd08c697b428f29ca
yumemi-omi <hiro.041310402.kt@gmail.com>
2023-03-18 12:22:39 +0900

    feat: when user clicks tag, fire ga event for analytics

---
+++ src/features/gtm/utils/eventTrigger.ts
@@ -0,0 +1,8 @@
+import { TagType } from "@/features/tag/types";
+
+export const fireClickTagTrigger = (tag: TagType) => {
+  window.dataLayer.push({
+    event: "click_tag",
+    click_tag_trigger: tag.name,
+  });
+};
