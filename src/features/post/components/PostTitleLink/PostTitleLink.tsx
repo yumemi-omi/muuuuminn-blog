3fd3030f7bb5f05a302175d64a0d59bc7cd34aee
yumemi-omi <hiro.041310402.kt@gmail.com>
2023-05-01 09:59:15 +0900

    fix: title link color

--- src/features/post/components/PostTitleLink/PostTitleLink.tsx
+++ src/features/post/components/PostTitleLink/PostTitleLink.tsx
@@ -10,34 +10,31 @@ type PostTitleProps = {
 
 const _PostTitleLink: FC<PostTitleProps> = ({ post }) => {
   return (
-    <CustomNextLink
-      href={`/post/${post.slug}`}
-      prefetch={false}
-      sx={(theme) => ({
-        textDecoration: "none",
-        color:
-          theme.colorScheme === "dark"
-            ? theme.colors["light-coral"][1]
-            : theme.colors["light-coral"][8],
-        transitionProperty: "var(--transition-property-common)",
-        transitionDuration: "var(--transition-duration-common)",
-        background: "linear-gradient(currentColor 0 0) 0 100% /var(--d, 0) 1px no-repeat",
-        "&:hover": {
-          color: theme.colorScheme === "dark" ? "#fec8c8cc" : "#775f5fcc",
-          "--d": "100%",
-        },
-        "&:visited": {
-          color:
-            theme.colorScheme === "dark"
-              ? theme.colors["light-coral"][1]
-              : theme.colors["light-coral"][8],
-        },
-      })}
-    >
-      <Text fz={"lg"} lineClamp={2} weight={"bold"}>
+    <Text fz={"lg"} lineClamp={2} weight={"bold"}>
+      <CustomNextLink
+        href={`/post/${post.slug}`}
+        prefetch={false}
+        sx={(theme) => ({
+          textDecoration: "none",
+          color: theme.colorScheme === "dark" ? theme.white : theme.black,
+          transitionProperty: "var(--transition-property-common)",
+          transitionDuration: "var(--transition-duration-common)",
+          background: "linear-gradient(currentColor 0 0) 0 100% /var(--d, 0) 1px no-repeat",
+          "&:hover": {
+            color: theme.colorScheme === "dark" ? "#fec8c8cc" : "#775f5fcc",
+            "--d": "100%",
+          },
+          "&:visited": {
+            color:
+              theme.colorScheme === "dark"
+                ? theme.colors["light-coral"][1]
+                : theme.colors["light-coral"][8],
+          },
+        })}
+      >
         {post.title}
-      </Text>
-    </CustomNextLink>
+      </CustomNextLink>
+    </Text>
   );
 };
 
